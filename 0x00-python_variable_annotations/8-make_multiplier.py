5d2354df96002222d07110f52941673695c92868
Joelayo <joetech.ooj@gmail.com>
2023-01-06 18:41:44 +0100

    Task 8 Complete

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''import List and Union module'''
+from typing import Callable
+
+'''
+Module for make_multiplier function
+'''
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply_by_multiplier(x: float) -> float:
+        return multiplier * x
+    return multiply_by_multiplier

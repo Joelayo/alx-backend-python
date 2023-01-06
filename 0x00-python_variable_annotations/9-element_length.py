20620aec90a9f1ceb54ea6f7e04fc83152952721
Joelayo <joetech.ooj@gmail.com>
2023-01-06 18:52:48 +0100

    Task 9 Complete

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''import List and Tuple module'''
 from typing import List, Tuple
+
 '''
 Module for combine_strings function
 '''
@@ -10,4 +11,3 @@ def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     '''Computes the length of a list of sequences.
     '''
     return [(i, len(i)) for i in lst]
-

fa1688649dfc9a61afed2f9b743c08e129ee06f3
Joelayo <joetech.ooj@gmail.com>
2023-01-06 18:42:59 +0100

    Task 8 Complete

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,6 +8,8 @@ Module for make_multiplier function
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''Creates a multiplier function.
+    '''
     def multiply_by_multiplier(x: float) -> float:
         return multiplier * x
     return multiply_by_multiplier

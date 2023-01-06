b33b03bae91b8baf72023726e7607e9be8fc58b9
Joelayo <joetech.ooj@gmail.com>
2023-01-06 18:28:59 +0100

    Task 5 Complete

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,6 +8,6 @@ Module for declared variable
 def sum_list(input_list: List[float]) -> float:
     '''Returns input_list sum as a float'''
     sum = 0.0
-    for sum in input_list:
-        result += sum
+    for i in input_list:
+        sum += i
     return sum
